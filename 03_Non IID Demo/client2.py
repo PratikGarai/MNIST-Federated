6bd0c278f13460e9f5f1524e6f3f643a54aaa9d7
RahulKaruparthi <108293302+RahulKaruparthi@users.noreply.github.com>
2022-12-08 16:11:46 +0530

    Update client2.py

--- 03_Non IID Demo/client2.py
+++ 03_Non IID Demo/client2.py
@@ -7,58 +7,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # AUxillary methods
-def getDist(y):
-    ax = sns.countplot(y)
-    ax.set(title="Count of data classes")
-    plt.show()
-
-def getData(dist, x, y):
-    dx = []
-    dy = []
-    counts = [0 for i in range(10)]
-    for i in range(len(x)):
-        if counts[y[i]]<dist[y[i]]:
-            dx.append(x[i])
-            dy.append(y[i])
-            counts[y[i]] += 1
-        
-    return np.array(dx), np.array(dy)
-
-# Load and compile Keras model
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28,28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(256, activation='relu'),
-    keras.layers.Dense(10, activation='softmax')
-])
-model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-
-# Load dataset
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train, x_test = x_train[..., np.newaxis]/255.0, x_test[..., np.newaxis]/255.0
-dist = [0, 10, 10, 10, 4000, 3000, 4000, 5000, 10, 4500]
-x_train, y_train = getData(dist, x_train, y_train)
-getDist(y_train)
-
-# Define Flower client
+#Define Flower client
 class FlowerClient(fl.client.NumPyClient):
     def get_parameters(self,config):
         return model.get_weights()
 
-    def fit(self, parameters, config):
-        model.set_weights(parameters)
-        r = model.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test), verbose=0)
-        hist = r.history
-        print("Fit history : " ,hist)
-        return model.get_weights(), len(x_train), {}
+#     def fit(self, parameters, config):
+#         model.set_weights(parameters)
+#         r = model.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test), verbose=0)
+#         hist = r.history
+#         print("Fit history : " ,hist)
+#         return model.get_weights(), len(x_train), {}
+    def fit(self):       #, parameters, config):
+        result = funcs.fit(swift_data_path = swift_data_path,
+            bank_data_path = bank_data_path,
+            model_dir = model_dir,
+            preds_format_path = preds_format_path,
+            preds_dest_path = preds_dest_path,
+            m = 'xgboost')
+        return result
 
-    def evaluate(self, parameters, config):
-        model.set_weights(parameters)
-        loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
-        print("Eval accuracy : ", accuracy)
-        return loss, len(x_test), {"accuracy": accuracy}
-
-# Start Flower client
+#     def evaluate(self, parameters, config):
+    def evaluate(self):     #, parameters, config):
+#         model.set_weights(parameters)
+#         loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
+#         print("Eval accuracy : ", accuracy)
+        result = funcs.predict(
+            swift_data_path =swift_data_path,
+            bank_data_path = bank_data_path,
+            model_dir = model_dir,
+            preds_format_path = preds_format_path,
+            preds_dest_path = preds_dest_path,
+            m = 'xgboost'
+            )  
+        #return loss, len(x_test), {"accuracy": accuracy}
+        return result
+    
+    # Start Flower client
 fl.client.start_numpy_client(
         server_address="localhost:"+str(sys.argv[1]), 
         client=FlowerClient(), 
